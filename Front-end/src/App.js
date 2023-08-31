675979db2663f56eef6cccc5a838a5b4527b8689
Hammad890 <hammadbro6@gmail.com>
2023-08-31 12:23:29 +0500

    ui start

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,12 +1,22 @@
-
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Dashboard/>
+     <Navbar/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+function App() {
+  
+
+ 
