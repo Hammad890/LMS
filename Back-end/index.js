b287a9a9e07b591fb692ba7c4b4818e52e24fc03
Hammad890 <126285257+Hammad890@users.noreply.github.com>
2024-05-29 10:52:51 +0500

    Update index.js

--- Back-end/index.js
+++ Back-end/index.js
@@ -35,6 +35,4 @@ app.use("/books",bookRouter)
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from Vercel' });
 });
-app.listen(5000, ()=>{
-    console.log("App started")
-})
+
