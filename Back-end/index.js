795decabaf7002f190d9aed93c483f132fb2b2df
Hammad890 <126285257+Hammad890@users.noreply.github.com>
2024-05-29 10:41:05 +0500

    updated

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,9 +1,11 @@
 import express, {json} from "express";
-import {callDb,config} from "./helpers/db.js";
+import {callDb} from "./helpers/db.js";
 import userRouter from "./routes/users.js";
 import bookRouter from "./routes/books.js";
 import cors from "cors"
 import session from "express-session";
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const app= express();
@@ -30,6 +32,9 @@ app.use(
 
 app.use("/users",userRouter)
 app.use("/books",bookRouter)
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from Vercel' });
+});
 app.listen(5000, ()=>{
     console.log("App started")
 })
