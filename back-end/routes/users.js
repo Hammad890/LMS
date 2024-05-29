2a52f23b0916d00770e474b507ba220f2bf7c896
Hammad890 <hammadbro6@gmail.com>
2024-05-29 10:38:57 +0500

    updated

---
+++ back-end/routes/users.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {getUser,bookToBorrow,bookReturn,login,showUser,logout,borrowedBook, createUser,deleteUser,getById,getBorrowedBook} from "../controller/user.js"
+
+const route = Router()
+
+route.get("/",getUser)
+route.post("/borrow",bookToBorrow)
+route.post("/return",bookReturn)
+route.post("/",login)
+route.get("/user",showUser)
+route.get("/logout",logout)
+route.get("/borrowed",borrowedBook)
+route.get("/borrowedbooks",getBorrowedBook)
+route.post("/register",createUser)
+route.delete("/:id",deleteUser)
+route.get("/:id",getById)
+
+export default route;
