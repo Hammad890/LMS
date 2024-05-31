b2ac4c6ed3d8c5fcbc4ac40beaa028d8c100245b
Hammad890 <126285257+Hammad890@users.noreply.github.com>
2024-05-31 11:13:17 +0500

    updated

--- Front-end/src/context.js
+++ Front-end/src/context.js
@@ -47,7 +47,7 @@ const UserProvider= ({children})=>{
   const loginUser = async(loginData)=>{
     try{
       console.log('Login Request:', loginData);
-        const response = await fetch ('https://lms-smoky-one.vercel.app//users',{
+        const response = await fetch ('https://lms-smoky-one.vercel.app/users',{
             method : 'POST',
             headers: {
                 'Content-Type':'application/json'
