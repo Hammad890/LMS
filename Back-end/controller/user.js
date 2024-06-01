01bf9a1f18dfca1a5da3bda1b521eb4fbfb18739
Hammad890 <126285257+Hammad890@users.noreply.github.com>
2024-06-01 10:07:25 +0500

    updated

--- Back-end/controller/user.js
+++ Back-end/controller/user.js
@@ -182,7 +182,7 @@ export const login= async (req,res,next)=>{
         if (user.role === "Admin" && user.username !== "admin") {
             return res.status(401).json({ error: "Admin does not exist" });
         }
-        req.session.userId= user._id;
+        req.session.userId= user.id;
         return res.status(200).json({user: ignorePassword(user.toJSON())})
     }catch(err){
         next (err)
